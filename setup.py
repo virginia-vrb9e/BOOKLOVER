cfb92e43626020cf5feb7b9f0b307f07648b32ca
VirginiaRuth_Brame <vrb9e@virginia.edu>
2025-03-23 21:59:40 -0400

    hopefully this works...

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='booklover',
+    version='1.0.0',
+    url='https://github.com/virginia-vrb9e/BOOKLOVER',
+    author='Virginia R. Brame',
+    author_email='vrb9e@virginia.edu',
+    description='This is the booklovers package.  It allows a user to create a lits of books with ratings for each entered book.',
+    packages=find_packages(),
+    license = 'LICENSE.txt'
+)
