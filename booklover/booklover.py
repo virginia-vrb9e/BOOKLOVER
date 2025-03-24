0f0b13ba63770045676c2f17926dd71ce892bbc0
VirginiaRuth_Brame <vrb9e@virginia.edu>
2025-03-23 23:13:42 -0400

    another commit

--- booklover/booklover.py
+++ booklover/booklover.py
@@ -1,79 +1,70 @@
 import numpy as np
 import pandas as pd
-
-class BookLover:
-    """
-    This class creates a database of contact information and beloved books
-    for book lovers (users). 
-    Attributes:
-    ----------
-    name :      (type:string) user's name
-    email :     (type:string) user's email
-    fav_genre : (type:string) user's favorite genre; (default=None)
-    num_books : (type:int) number of books user has read/logged
-    book_list : (type:pd.DataFrame) a DataFrame of:
+    
+"""
+This class creates a database of contact information and beloved books
+for book lovers (users). 
+Attributes:
+----------
+name :      (type:string) user's name
+email :     (type:string) user's email
+fav_genre : (type:string) user's favorite genre; (default=None)
+num_books : (type:int) number of books user has read/logged
+book_list : (type:pd.DataFrame) a DataFrame of:
                 book titles (type:string) and 
                 ratings (type: integer) provided by the user 
                 on a scale of 1 - 5: 
                     1(lowest rating) 
                     5(highest rating)
-    Methods:
-    --------
-    """
-
-    # constructor
-    def __init__(self, name, email, fav_genre= None):
+"""
+class BookLover:
+    def __init__(self, name, email, fav_genre):  # constructor
         self.name = name
         self.email = email
         self.fav_genre = fav_genre
-        num_books = 0  # initialize at zero, then add
-        book_list = pd.DataFrame({'book_name': [], 'book_rating': []})
-   
-    # Method 1: add_book             
-    def add_book(self, book_name, book_rating):
+        self.book_list = pd.DataFrame({'book_name': [], 'book_rating': []})
+        self.num_books = 0 # initialize at zero
+        
+    def add_book(self, book_name, book_rating):  #Method #1: add_book
+        
         """
-        Takes a book title and a numeric rating from 1 to 5 and adds it to a unique list of books.
+        Takes a book title and adds it to a unique list of books.
         Parameters
         ----------
         book_name :  str
         book_rating : int
             a rating from 1 - 5 with 1 being the lowest rating and 5 the highest
-        Raises
-        ------
-        none
         """
         
-        new_book = pd.DataFrame({'book_name': [book_name], 'book_rating': [book_rating]})
+        if book_name in self.book_list["book_name"].values: #indexing into dictionary key vector
+            print(f"You've already added '{book_name}' to your list.")
+            return
+        
+        # add book
+        new_book = pd.DataFrame({'book_name': [book_name],'book_rating': [book_rating]})
         self.book_list = pd.concat([self.book_list, new_book], ignore_index=True)
-        #self.book_name = book_name
-        #self.book_rating = book_rating
-        #new_book_input = {book_name : book_rating}
-        if book_name in self.book_list:
-            print("You already put that book in your list. Choose another one.")
-        else:
-            # self.book_list.update(new_book_input)
-            self.book_list = pd.concat([self.book_list, new_book], ignore_index=True)
-            # return book_list
+        print(f"'{book_name}' with a rating of {book_rating} has been added to your list.")
+        self.num_books += 1
+        return self.book_list # this makes the DF print out. 
 
-    # Method 2: has_read
-    def has_read(self, book_list, book_name):
+        
+    def has_read(self, new_book_name):  # Method 2: has_read
+  
         """
         Checks to see if a book title has already been added to the list
         Parameters
         ----------
         book_name : str
             a book title
-        Raises
-        ------
-        none
         """
-        if book_name in book_list:
+    
+        if new_book_name in self.book_list["book_name"].values:
             return True
+            # print('True')
         else:
             return False
-     
-    # Method 3: num_books_read   
-    def num_books_read(book_list):
+
+    def num_books_read(self):  # Method 3: num_books_read   
         """
         This method is a static method 
         It returns a value that is the number of books read 
@@ -82,10 +73,9 @@ class BookLover:
         -------
         num_books_read : int
         """
-        return len(book_list)
+        print(self.num_books)       
 
-    # Method 4: fav_books
-    def fav_books(book_rating, book_list):
+    def fav_books(self):  # Method 4: fav_books
         """
         This is another static method. 
         It returns a filtered DataFrame
@@ -94,13 +84,59 @@ class BookLover:
         ------
         fav_books : pd.DataFrame
         """
-        fav_books = [x for x in book_list if x > 3]
-        return pd.DataFrame(fav_books)
-
-
-
-if __name__ == '__main__':
+        fav_books_list = []
+        for index, row in self.book_list.iterrows():  # df.iterrows()
+            if (row['book_rating'] > 3):
+                fav_books_list.append(row['book_name'])
+        return fav_books_list
     
+if __name__ == '__main__':  
     test_object = BookLover("Han Solo", "hsolo@millenniumfalcon.com", "scifi")
     test_object.add_book("War of the Worlds", 4)
-    # And so forth  
+    test_object.has_read("The Martian")
+    test_object.num_books_read()
+    test_object.fav_books()
+    
+# Testing Class for Correct Functioning    
+#testing method #1    
+    # instantiate
+# booklover_mary = BookLover('Mary Lamb', 'maryhada@littlelamb.com', "Children's")
+# booklover_mary.name
+# booklover_mary.book_list
+# booklover_mary.book_list.book_name # ok
+# bk = np.array(booklover_mary.book_list.book_name) #ok
+# bk[-1] # ok
+
+#testing method #2
+# booklover_mary = BookLover('Mary Lamb', 'maryhada@littlelamb.com', "Children's")
+# booklover_mary.has_read('Lord of the Flies')
+# booklover_mary.has_read('A')
+
+# testing method #3
+# booklover_mary = BookLover('Mary Lamb', 'maryhada@littlelamb.com', "Children's")
+# booklover_mary.add_book('Middlemarch', 5)
+# booklover_mary.add_book('Fahrenheit 451', 5)
+# booklover_mary.add_book('To Kill a Mockingbird', 5)
+# booklover_peter = BookLover('Peter Parker', 'spidey@boynextdoor.com', "Travel")
+# booklover_peter.add_book('Harry Potter', 2)
+# booklover_peter.num_books_read()
+    
+
+# testing method #4
+# booklover_peter = BookLover('Mary Lamb', 'maryhada@littlelamb.com', "Children's")
+# booklover_peter.add_book('Harry Potter IV', 2)
+# booklover_peter.add_book('Lord of the Flies IV', 4)
+# booklover_peter.add_book('Leaves of Grass IV', 5)
+# booklover_peter.fav_books()
+
+# https://stackoverflow.com/questions/40659212/futurewarning-elementwise-comparison-failed-returning-scalar-but-in-the-futur
+                 
+                 
+                 
+                 
+                 
+                 
+                 
+                 
+                 
+            
--- booklover_test.py
+++ booklover_test.py
@@ -15,49 +15,61 @@ class BookLoverTestSuite(unittest.TestCase):
     def test_1_add_book(self): 
         
         # initiate the class:
-        book_list_test = BookLover("Harry Potter", "potter@hogwarts.co.uk", "Fantasy")
+        booklover_test = BookLover("Harry Potter", "potter@hogwarts.co.uk", "Fantasy")
         # add a book:
-        book_list_test.add_book("Charlotte's Web", 2)
+        booklover_test.add_book("Charlotte's Web", 2)
         # check if it was added to book_list:
         # self.assertIn(value, container, message)
-        self.assertIn("Charlottes's Web", self.book_list, "Unknown Error: The book wasn't added to your list for some reason.") 
+        self.assertIn("Charlotte's Web", booklover_test.book_list, "Unknown Error: The book wasn't added to your list for some reason.") 
         
         
     def test_2_add_book(self):  
+        booklover_test = BookLover("Harry Potter", "potter@hogwarts.co.uk", "Fantasy")
+
         # add the same book twice. 
         booklover_test.add_book("Mary Poppins", 4)
         booklover_test.add_book("Mary Poppins", 4)
         # test if it's in `book_list` only once.
-        book_names = self.book_list.loc[:, "book_name"]
-        assertTrue(book_names.is_unique)
+        book_names = booklover_test.book_list.loc[:, "book_name"]
+        self.assertTrue(book_names.is_unique)
         
         
     def test_3_has_read(self): 
+        booklover_test = BookLover("Harry Potter", "potter@hogwarts.co.uk", "Fantasy")
+
         # pass a book in the list & test if answer is `True`.
         booklover_test.add_book("ABC", 1)
         self.assertTrue(booklover_test.has_read("ABC"))
-        self.assertTrue(booklover_test.has read(booklover_test.book_list.iloc[0,0]))
+        self.assertTrue(booklover_test.has_read(booklover_test.book_list.iloc[2,0]))
 
         
     def test_4_has_read(self): 
+        booklover_test = BookLover("Harry Potter", "potter@hogwarts.co.uk", "Fantasy")
+        booklover_test.add_book("The Stand", 5)
+
         # pass a book NOT in the list and use `assert False` to test the answer is `True`
-        value = self.book_list.iloc[0,0]
+        value = booklover_test.book_list.iloc[0,0]
         self.assertFalse(booklover_test.has_read(value[:5]))
 
     def test_5_num_books_read(self): 
-        # add some books to the list, and test num_books matches expected.
+        booklover_test = BookLover("Harry Potter", "potter@hogwarts.co.uk", "Fantasy")
+
+        # add some books to the list, and test if num_books matches expected value
         booklover_test.add_book("The Stand", 5)
         booklover_test.add_book("The Haunting of Hill House", 5)
-        self.assertEqual(booklover_test.num_books == 5)
+        self.assertEqual(booklover_test.num_books == 2)
         
     def test_6_fav_books(self):
+        booklover_test = BookLover("Harry Potter", "potter@hogwarts.co.uk", "Fantasy")
+
         # add some books with ratings to the list, making sure some of them have rating > 3. 
         booklover_test.add_book("Midnight in the Garden of Good and Evil", 4)
         booklover_test.add_book("Lord of the Rings", 5)
-        booklover_test.add_book("Clear and Present Danger", 2)
+        booklover_test.add_book("A Clear and Present Danger", 2)
         # Your test should check that the returned books have rating  > 3   
-        self
+        ratings = [row['book_rating'] for index, row in booklover_test.book_list.iterrows() if (row["book_rating"] > 3)]
+        self.assertGreater(ratings, 3, "not all fav_books have a rating >3")
         
-                
+        
 if __name__ == '__main__':
-    unittest.main(verbosity=3)
+    unittest.main(argv=['first-arg-is-ignored'],exit=False)
